8689273f72e942110cee043f1e6e549052cb99a8
S00227188 <S00227188@atu.ie>
2023-11-09 15:40:43 +0000

    Added county select to register page

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import { MDBContainer, MDBInput, MDBBtn } from "mdb-react-ui-kit";
 import UserPool from "../Cognito";
-import { CognitoUserAttribute } from "amazon-cognito-identity-js"; // Import CognitoUserAttribute
+import { CognitoUserAttribute } from "amazon-cognito-identity-js";
 
 interface FormData {
   username: string;
   email: string;
   password: string;
+  county: string;
 }
 
 const Register: React.FC = () => {
@@ -14,9 +15,12 @@ const Register: React.FC = () => {
     username: "",
     email: "",
     password: "",
+    county: "",
   });
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
     const { name, value } = event.target;
     setFormData((prevState) => ({ ...prevState, [name]: value }));
   };
@@ -80,6 +84,52 @@ const Register: React.FC = () => {
               value={formData.password}
               onChange={handleChange}
             />
+          </div>
+          <div className="mb-4">
+            <select
+              className="form-select"
+              id="county"
+              name="county"
+              value={formData.county}
+              onChange={handleChange}
+              style={{ color: formData.county === "" ? "#6c757d" : "#495057" }}
+            >
+              <option value="" disabled>
+                Select County
+              </option>
+              <option value="Antrim">Antrim</option>
+              <option value="Armagh">Armagh</option>
+              <option value="Carlow">Carlow</option>
+              <option value="Cavan">Cavan</option>
+              <option value="Clare">Clare</option>
+              <option value="Cork">Cork</option>
+              <option value="Derry">Derry</option>
+              <option value="Donegal">Donegal</option>
+              <option value="Down">Down</option>
+              <option value="Dublin">Dublin</option>
+              <option value="Fermanagh">Fermanagh</option>
+              <option value="Galway">Galway</option>
+              <option value="Kerry">Kerry</option>
+              <option value="Kildare">Kildare</option>
+              <option value="Kilkenny">Kilkenny</option>
+              <option value="Laois">Laois</option>
+              <option value="Leitrim">Leitrim</option>
+              <option value="Limerick">Limerick</option>
+              <option value="Longford">Longford</option>
+              <option value="Louth">Louth</option>
+              <option value="Mayo">Mayo</option>
+              <option value="Meath">Meath</option>
+              <option value="Monaghan">Monaghan</option>
+              <option value="Offaly">Offaly</option>
+              <option value="Roscommon">Roscommon</option>
+              <option value="Sligo">Sligo</option>
+              <option value="Tipperary">Tipperary</option>
+              <option value="Tyrone">Tyrone</option>
+              <option value="Waterford">Waterford</option>
+              <option value="Westmeath">Westmeath</option>
+              <option value="Wexford">Wexford</option>
+              <option value="Wicklow">Wicklow</option>
+            </select>
           </div>
           <div className="text-center mt-4">
             <MDBBtn color="primary" type="submit">
