17737cbcc965f77d3abac8e1e004ea260076e6f2
s00227414 <s00227414@atu.ie>
2023-12-04 22:52:20 +0000

    Added basic Suggested veg card with mock data.

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -9,52 +9,96 @@ interface WeatherData {
   soil: { '#text': string; '@units': string };
   // Add other fields if needed
 }
+interface VegetableData {
+    name: string;
+    image: string;
+    description: string;
+    growTime: string;
+    season: string;
+    // Add other fields if needed
+  }
 
 const HomePage: React.FC = () => {
   const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
+  const [vegetableData, setVegetableData] = useState<VegetableData | null>(null);
 
   useEffect(() => {
-    const fetchData = async () => {
+    // Fetch weather data
+    const fetchWeatherData = async () => {
       try {
         const response = await fetch('https://prodapi.metweb.ie/agriculture/report');
         const data = await response.json();
         const station = data.report.station.find((station: WeatherData) => station['@name'] === 'Knock Airport');
         setWeatherData(station);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error('Error fetching weather data:', error);
       }
     };
 
-    fetchData();
+    // Fetch vegetable data
+    const fetchVegetableData = async () => {
+      try {
+        // Fetch vegetable data from your API or source
+        // Example: const response = await fetch('https://your-vegetable-api.com');
+        // const data = await response.json();
+        // setVegetableData(data);
+
+        // For now, setting dummy vegetable data
+        setVegetableData({
+          name: 'Tomato',
+          image: 'tomato.jpg',
+          description: 'A red fruit that is used in many culinary dishes.',
+          growTime: '80 days',
+          season: 'Winter'
+        });
+      } catch (error) {
+        console.error('Error fetching vegetable data:', error);
+      }
+    };
+
+    fetchWeatherData();
+    fetchVegetableData();
   }, []);
 
   return (
-    <MDBContainer className="py-5 d-flex flex-column align-items-center">
-  <h1 className="mb-4">Home</h1>
-  <div className="d-flex justify-content-center">
-    {weatherData && (
-      <MDBCard style={{ width: '20rem', margin: '1rem' }}>
-        <MDBCardBody>
-          <MDBCardTitle>Weather at {weatherData['@name']}</MDBCardTitle>
-          <MDBCardText>
-            Temperature: {weatherData.temp['#text']}°{weatherData.temp['@unit']}
-          </MDBCardText>
-          <MDBCardText>
-            Rainfall: {weatherData.rain['#text']} {weatherData.rain['@unit']}
-          </MDBCardText>
-          <MDBCardText>
-            Wind: {weatherData.wind['#text']} {weatherData.wind['@units']}
-          </MDBCardText>
-          <MDBCardText>
-            Soil Temp: {weatherData.soil['#text']} {weatherData.soil['@units']}
-          </MDBCardText>
-          {/* Add more fields if needed */}
-        </MDBCardBody>
-      </MDBCard>
-    )}
-  </div>
-</MDBContainer>
+    <div>
+      <MDBContainer className="py-5 d-flex justify-content-center align-items-center">
+        {weatherData && (
+          <MDBCard style={{ width: '20rem', margin: '1rem' }}>
+            <MDBCardBody>
+              <MDBCardTitle>Weather at {weatherData['@name']}</MDBCardTitle>
+              <MDBCardText>
+                Temperature: {weatherData.temp['#text']}°{weatherData.temp['@unit']}
+              </MDBCardText>
+              <MDBCardText>
+                Rainfall: {weatherData.rain['#text']} {weatherData.rain['@unit']}
+              </MDBCardText>
+              <MDBCardText>
+                Wind: {weatherData.wind['#text']} {weatherData.wind['@units']}
+              </MDBCardText>
+              <MDBCardText>
+                Soil Temp: {weatherData.soil['#text']} {weatherData.soil['@units']}
+              </MDBCardText>
+              {/* Add more fields if needed */}
+            </MDBCardBody>
+          </MDBCard>
+        )}
+      </MDBContainer>
 
+      {vegetableData && (
+        <MDBContainer className="py-5 d-flex justify-content-center align-items-center">
+          <MDBCard style={{ width: '20rem', margin: '1rem' }}>
+            <MDBCardBody>
+            <MDBCardTitle>Suggested Vegetable in {vegetableData.season}</MDBCardTitle>
+              <MDBCardTitle>{vegetableData.name}</MDBCardTitle>
+              <MDBCardText>{vegetableData.description}</MDBCardText>
+              <MDBCardText>Grow Time: {vegetableData.growTime}</MDBCardText>
+              {/* Render the image here */}
+            </MDBCardBody>
+          </MDBCard>
+        </MDBContainer>
+      )}
+    </div>
   );
 };
 
