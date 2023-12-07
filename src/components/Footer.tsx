8d526b1ae1da1c84abb4475273fe03e8756c6a5c
S00227188 <S00227188@atu.ie>
2023-12-07 16:49:58 +0000

    Add social media links to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,23 +1,50 @@
-import { MDBFooter } from "mdb-react-ui-kit";
+import { MDBFooter, MDBContainer, MDBIcon, MDBBtn } from "mdb-react-ui-kit";
 
 export default function App() {
   return (
-    <div
-      style={{ display: "flex", flexDirection: "column", minHeight: "50vh" }}
-    >
-      <div style={{ flex: 1 }}>{/* Your main content goes here */}</div>
-      <MDBFooter
-        bgColor="light"
-        className="text-center text-lg-left"
-        style={{ marginTop: "auto" }}
+    <MDBFooter className="bg-light text-center text-white">
+      <MDBContainer className="p-4 pb-0">
+        <section className="mb-4">
+          <MDBBtn
+            floating
+            className="m-1"
+            style={{ backgroundColor: "#3b5998" }}
+            href="#!"
+            role="button"
+          >
+            <MDBIcon fab icon="facebook-f" />
+          </MDBBtn>
+
+          <MDBBtn floating className="m-1" href="#!" role="button">
+            <MDBIcon fab icon="twitter" />
+          </MDBBtn>
+
+          <MDBBtn floating className="m-1" href="#!" role="button">
+            <MDBIcon fab icon="google" />
+          </MDBBtn>
+          <MDBBtn floating className="m-1" href="#!" role="button">
+            <MDBIcon fab icon="instagram" />
+          </MDBBtn>
+
+          <MDBBtn floating className="m-1" href="#!" role="button">
+            <MDBIcon fab icon="linkedin-in" />
+          </MDBBtn>
+
+          <MDBBtn floating className="m-1" href="#!" role="button">
+            <MDBIcon fab icon="github" />
+          </MDBBtn>
+        </section>
+      </MDBContainer>
+
+      <div
+        className="text-center p-3 text-dark"
+        style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
       >
-        <div className="text-center p-3">
-          &copy; {new Date().getFullYear()} Copyright:{" "}
-          <a className="text-dark" href="#">
-            GoGrow.com
-          </a>
-        </div>
-      </MDBFooter>
-    </div>
+        © 2022 Copyright:
+        <a className="text-dark" href="#">
+          GoGrow
+        </a>
+      </div>
+    </MDBFooter>
   );
 }
