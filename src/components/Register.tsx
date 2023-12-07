dc365818f51c3a3a5147667600e7b74ab1573498
S00227188 <S00227188@atu.ie>
2023-12-07 17:41:13 +0000

    Fix register

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -51,7 +51,7 @@ const Register: React.FC = () => {
           console.error("Error during registration:", err);
         } else {
           console.log("Registration successful:", data);
-          navigate("/login"); // Redirect to login page
+          navigate("/login");
         }
       }
     );
@@ -105,7 +105,41 @@ const Register: React.FC = () => {
               onChange={handleChange}
               style={{ color: formData.county === "" ? "#6c757d" : "#495057" }}
             >
-              {/* options */}
+              <option value="" disabled>
+                Select County
+              </option>
+              <option value="Antrim">Antrim</option>
+              <option value="Armagh">Armagh</option>
+              <option value="Carlow">Carlow</option>
+              <option value="Cavan">Cavan</option>
+              <option value="Clare">Clare</option>
+              <option value="Cork">Cork</option>
+              <option value="Derry">Derry</option>
+              <option value="Donegal">Donegal</option>
+              <option value="Down">Down</option>
+              <option value="Dublin">Dublin</option>
+              <option value="Fermanagh">Fermanagh</option>
+              <option value="Galway">Galway</option>
+              <option value="Kerry">Kerry</option>
+              <option value="Kildare">Kildare</option>
+              <option value="Kilkenny">Kilkenny</option>
+              <option value="Laois">Laois</option>
+              <option value="Leitrim">Leitrim</option>
+              <option value="Limerick">Limerick</option>
+              <option value="Longford">Longford</option>
+              <option value="Louth">Louth</option>
+              <option value="Mayo">Mayo</option>
+              <option value="Meath">Meath</option>
+              <option value="Monaghan">Monaghan</option>
+              <option value="Offaly">Offaly</option>
+              <option value="Roscommon">Roscommon</option>
+              <option value="Sligo">Sligo</option>
+              <option value="Tipperary">Tipperary</option>
+              <option value="Tyrone">Tyrone</option>
+              <option value="Waterford">Waterford</option>
+              <option value="Westmeath">Westmeath</option>
+              <option value="Wexford">Wexford</option>
+              <option value="Wicklow">Wicklow</option>
             </select>
           </div>
           <div className="text-center mt-4">
