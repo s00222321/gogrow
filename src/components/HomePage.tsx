229f36838f9949f244641298515b9f6b47f15266
s00222321 <s00222321@atu.ie>
2023-12-07 14:15:27 +0000

    fixed home page

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { MDBContainer, MDBCard, MDBCardBody, MDBCardTitle, MDBCardText, MDBCol, MDBRow } from 'mdb-react-ui-kit';
+import { MDBCard, MDBCardBody, MDBCardTitle, MDBCardText } from 'mdb-react-ui-kit';
 
 interface WeatherData {
   '@name': string;
@@ -21,174 +21,138 @@ interface VegetableData {
   growtime: string;
 }
 
-
-
 const HomePage: React.FC = () => {
   const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
-  const [vegetableData, setVegetableData] = useState<VegetableData | null>(null);
-  const [SelectedVegetable, setSelectedVegetable] = useState<VegetableData | null>(null);
-  const [currentSeason, setCurrentSeason] = useState('');
+  const [selectedVegetable, setSelectedVegetable] = useState<VegetableData | null>(null);
+  const [sensorData, setSensorData] = useState<any[] | null>(null);
 
   useEffect(() => {
-    const fetchWeatherData = async () => {
+    const fetchData = async () => {
       try {
-        const response = await fetch('https://prodapi.metweb.ie/agriculture/report');
-        const data = await response.json();
-        const station = data.report.station.find((station: WeatherData) => station['@name'] === 'Knock Airport');
+        // Fetch weather data
+        const weatherResponse = await fetch('https://prodapi.metweb.ie/agriculture/report');
+        const weatherData = await weatherResponse.json();
+        const station = weatherData.report.station.find((station: WeatherData) => station['@name'] === 'Knock Airport');
         setWeatherData(station);
+
+        // Determine current season
+        const date = new Date();
+        const month = date.getMonth() + 1;
+        let season;
+
+        if (month >= 3 && month <= 5) {
+          season = 'spring';
+        } else if (month >= 6 && month <= 8) {
+          season = 'summer';
+        } else if (month >= 9 && month <= 11) {
+          season = 'autumn';
+        } else {
+          season = 'winter';
+        }
+
+        // Fetch plant data based on the current season
+        const plantDataResponse = await fetch(`https://bmhnryodyk.execute-api.eu-west-1.amazonaws.com/v1/season/${season}`);
+        const responseData = await plantDataResponse.json();
+
+        // Check if the response data has a 'body' property
+        if (responseData.body) {
+          const plantData = JSON.parse(responseData.body);
+
+          let randomIndex, randomPlant;
+          if (plantData.length > 1) {
+            randomIndex = Math.floor(Math.random() * plantData.length);
+            randomPlant = plantData[randomIndex];
+          } else {
+            randomPlant = plantData[0];
+          }
+          setSelectedVegetable(randomPlant);
+        } else {
+          console.error('Error: Response data does not contain a body property');
+        }
+
+        // Fetch sensor data
+        const sensorResponse = await fetch('https://loxs1vvtc3.execute-api.eu-west-1.amazonaws.com/v1/SoilMoisture');
+        const sensorData = await sensorResponse.json();
+        
+        // Log the raw sensor data
+        console.log('Raw Sensor Data:', sensorData);
+        
+        // Check if the sensor data is not empty
+        if (sensorData && sensorData.length > 0) {
+          // Get the most recent sensor reading
+          const mostRecentReading = sensorData[sensorData.length - 1];
+          setSensorData(sensorData);
+        
+          // Log the most recent reading
+          console.log('Most Recent Sensor Reading:', mostRecentReading);
+        
+          // You can use mostRecentReading.reading_value as needed
+        } else {
+          console.error('Error: Sensor data is empty');
+        }
       } catch (error) {
-        console.error('Error fetching weather data:', error);
+        console.error('Error fetching data:', error);
       }
     };
 
-    const fetchVegetableData = async () => {
-      try {
-        const randomNumber = Math.floor(Math.random() * 25) + 1;
-        const endpoint = `https://bmhnryodyk.execute-api.eu-west-1.amazonaws.com/v1/${randomNumber}`;
-        const response = await fetch(endpoint);
-        const data = await response.json();
-        const parsedData = JSON.parse(data.body);
-        setVegetableData(parsedData);
-      } catch (error) {
-        console.error('Error fetching vegetable data:', error);
-      }
-    };
-
-    fetchWeatherData();
-    fetchVegetableData();
+    fetchData();
   }, []);
 
-  useEffect(() => {
-    const determineCurrentSeason = () => {
-      const date = new Date();
-      const month = date.getMonth() + 1;
-      let season;
-
-      if (month >= 3 && month <= 5) {
-        season = 'Spring';
-      } else if (month >= 6 && month <= 8) {
-        season = 'Summer';
-      } else if (month >= 9 && month <= 11) {
-        season = 'Autumn';
-      } else {
-        season = 'Winter';
-      }
-
-      setCurrentSeason(season);
-    };
-
-    determineCurrentSeason();
-  }, []);
-
-  useEffect(() => {
-    const fetchPlantData = async () => {
-      try {
-        const endpoint = `https://bmhnryodyk.execute-api.eu-west-1.amazonaws.com/v1/${generateRandomPlantId(currentSeason)}`;
-        const response = await fetch(endpoint);
-        const responseBody = await response.json();
-        console.log(responseBody)
-        setSelectedVegetable(responseBody);
-        console.log('Selected plant fetched:', responseBody);
-      } catch (error) {
-        console.error('Error fetching plant data:', error);
-      }
-    };
-
-    fetchPlantData();
-  }, [currentSeason]);
-
-  const generateRandomPlantId = (season: string) => {
-    if (season === 'Spring') {
-      return Math.floor(Math.random() * (8 - 1 + 1)) + 1;
-    } else if (season === 'Summer') {
-      return Math.floor(Math.random() * (18 - 9 + 1)) + 9;
-    } else if (season === 'Autumn') {
-      return Math.floor(Math.random() * (24 - 19 + 1)) + 19;
-    } else if (season === 'Winter') {
-      return 25; // Return the specific ID for Winter
-    }
-  
-    return 1; // Default to 1 if the season is not recognized
-  };
-  
   return (
-    <div className="d-flex justify-content-center align-items-center" style={{ minHeight: '100vh' }}>
-      <MDBContainer className="py-0">
-        <MDBRow className="justify-content-center">
-          {weatherData && (
-            <MDBCol md="6" lg="4" className="mb-3" style={{ margin: '1rem' }}>
-              {weatherData && (
-          <MDBCard style={{ width: '20rem', margin: '1rem' }}>
-            <MDBCardBody>
-              <MDBCardTitle>Weather at {weatherData['@name']}</MDBCardTitle>
-              <MDBCardText>
-                Temperature: {weatherData.temp['#text']}°{weatherData.temp['@unit']}
-              </MDBCardText>
-              <MDBCardText>
-                Rainfall: {weatherData.rain['#text']} {weatherData.rain['@unit']}
-              </MDBCardText>
-              <MDBCardText>
-                Wind: {weatherData.wind['#text']} {weatherData.wind['@units']}
-              </MDBCardText>
-              <MDBCardText>
-                Soil Temp: {weatherData.soil['#text']} {weatherData.soil['@units']}
-              </MDBCardText>
-            </MDBCardBody>
-          </MDBCard>
-        )}
-            </MDBCol>
-          )}
-  
-          {vegetableData && (
-            <MDBCol md="6" lg="4" className="mb-3" style={{ margin: '1rem' }}>
-               {vegetableData && (
-          <MDBCard style={{ width: '20rem', margin: '1rem' }}>
-            <MDBCardBody>
-            <MDBCardTitle>Suggested Vegetable for {currentSeason}</MDBCardTitle>
-              <MDBCardTitle>{vegetableData.name}</MDBCardTitle>
-              <img src={vegetableData.plants} alt={vegetableData.name} style={{ width: '100%' }} />
-              <MDBCardText>{vegetableData.description}</MDBCardText>
-              <MDBCardText>Season: {vegetableData.season}</MDBCardText>
-              <MDBCardText>Grow Time: {vegetableData.growtime}</MDBCardText>
-            </MDBCardBody>
-          </MDBCard>
-        )}
-            </MDBCol>
-          )}
-  
-          {SelectedVegetable && (
-            <MDBCol md="6" lg="4" className="mb-3" style={{ margin: '1rem' }}>
-              {SelectedVegetable && (
-  <>
-    {console.log('Selected Plant Object:', SelectedVegetable)}
-    <MDBCard style={{ width: '20rem', margin: '1rem' }}>
-      <MDBCardBody>
-        <MDBCardTitle>{SelectedVegetable.name}</MDBCardTitle>
-        <img src={SelectedVegetable.plants} alt={SelectedVegetable.name} style={{ width: '100%' }} />
-        <MDBCardText>{SelectedVegetable.description}</MDBCardText>
-        <MDBCardText>Season: {SelectedVegetable.season}</MDBCardText>
-        <MDBCardText>Grow Time: {SelectedVegetable.growtime}</MDBCardText>
-      </MDBCardBody>
-    </MDBCard>
-  </>
-)}
-            </MDBCol>
-          )}
-  
-          {/* Sensor Data Card */}
-          <MDBCol xs="12" style={{ margin: '1rem' }}>
-          <MDBCard style={{ width: '20rem', margin: '1rem' }}>
-            <MDBCardBody>
-              <MDBCardTitle>Sensor Data</MDBCardTitle>
-            </MDBCardBody>
-          </MDBCard>
-          </MDBCol>
-        </MDBRow>
-      </MDBContainer>
+    <div className="d-flex flex-column align-items-center" style={{ minHeight: '100vh' }}>
+      <h1 className="mt-3 mb-4">Welcome Siobhan 🌱</h1>
+
+      {weatherData && (
+        <MDBCard className="mb-3" style={{ width: '20rem', borderRadius: '8px', boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)' }}>
+          <MDBCardBody>
+            <MDBCardTitle className="mb-3">Weather at {weatherData['@name']}</MDBCardTitle>
+            <div className="d-flex justify-content-between mb-2">
+              <div>Temperature:</div>
+              <div>{weatherData.temp['#text']}°{weatherData.temp['@unit']}</div>
+            </div>
+            <div className="d-flex justify-content-between mb-2">
+              <div>Rainfall:</div>
+              <div>{weatherData.rain['#text']} {weatherData.rain['@unit']}</div>
+            </div>
+            <div className="d-flex justify-content-between mb-2">
+              <div>Wind:</div>
+              <div>{weatherData.wind['#text']} {weatherData.wind['@units']}</div>
+            </div>
+            <div className="d-flex justify-content-between">
+              <div>Soil Temp:</div>
+              <div>{weatherData.soil['#text']} {weatherData.soil['@units']}</div>
+            </div>
+          </MDBCardBody>
+        </MDBCard>
+      )}
+
+      {selectedVegetable && (
+        <MDBCard className="mb-3" style={{ width: '20rem' }}>
+          <img src={selectedVegetable.plants} alt={selectedVegetable.name} style={{ width: '100%', borderTopLeftRadius: '8px', borderTopRightRadius: '8px' }} />
+          <MDBCardBody>
+            <MDBCardTitle className="text-center mt-3 mb-3">Recommended Seasonal Veg</MDBCardTitle>
+            <MDBCardText className="text-center">
+              <strong>{selectedVegetable.name}</strong>
+            </MDBCardText>
+            <MDBCardText className="text-center">Season: {selectedVegetable.season}</MDBCardText>
+            <MDBCardText className="text-center">Grow Time: {selectedVegetable.growtime}</MDBCardText>
+          </MDBCardBody>
+        </MDBCard>
+      )}
+
+{sensorData && (
+        <MDBCard className="mb-3" style={{ width: '20rem' }}>
+          <MDBCardBody>
+            <MDBCardTitle>Soil Moisture Sensor</MDBCardTitle>
+            <MDBCardText>
+              Most Recent Reading: {sensorData[sensorData.length - 1].reading_value}
+            </MDBCardText>
+            {/* You can add more details as needed */}
+          </MDBCardBody>
+        </MDBCard>
+      )}
     </div>
   );
-
-    
 };
 
 export default HomePage;
