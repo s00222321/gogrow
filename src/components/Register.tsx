6ead0d98d8d8247f3c0fbaf5c30cadb25bb39a98
S00227188 <S00227188@atu.ie>
2023-12-07 17:37:57 +0000

    Make register redirect to login

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { MDBContainer, MDBInput, MDBBtn } from "mdb-react-ui-kit";
 import UserPool from "../Cognito";
 import { CognitoUserAttribute } from "amazon-cognito-identity-js";
+import { useNavigate } from "react-router-dom";
 
 interface FormData {
   username: string;
@@ -18,43 +19,43 @@ const Register: React.FC = () => {
     county: "",
   });
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+  const navigate = useNavigate();
+
+  const handleChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
     const { name, value } = event.target;
-    console.log(`Name: ${name}, Value: ${value}`);
     setFormData((prevState) => ({ ...prevState, [name]: value }));
-};
+  };
 
-const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-  event.preventDefault();
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
 
-  console.log("Form Data:", formData); // Log the form data before signup
-
-  const emailAttribute = new CognitoUserAttribute({
+    const emailAttribute = new CognitoUserAttribute({
       Name: "email",
       Value: formData.email,
-  });
+    });
 
-  // Include the county attribute in the user registration
-  const countyAttribute = new CognitoUserAttribute({
-      Name: "custom:county",  // Update this if your attribute is a custom attribute
+    const countyAttribute = new CognitoUserAttribute({
+      Name: "custom:county",
       Value: formData.county,
-  });
+    });
 
-  UserPool.signUp(
+    UserPool.signUp(
       formData.username,
       formData.password,
-      [emailAttribute, countyAttribute],  // Include the countyAttribute in the array
+      [emailAttribute, countyAttribute],
       [],
       (err, data) => {
-          if (err) {
-              console.error("Error during registration:", err);
-          } else {
-              console.log("Registration successful:", data);
-          }
+        if (err) {
+          console.error("Error during registration:", err);
+        } else {
+          console.log("Registration successful:", data);
+          navigate("/login"); // Redirect to login page
+        }
       }
-  );
-};
-
+    );
+  };
 
   return (
     <MDBContainer
@@ -104,41 +105,7 @@ const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
               onChange={handleChange}
               style={{ color: formData.county === "" ? "#6c757d" : "#495057" }}
             >
-              <option value="" disabled>
-                Select County
-              </option>
-              <option value="Antrim">Antrim</option>
-              <option value="Armagh">Armagh</option>
-              <option value="Carlow">Carlow</option>
-              <option value="Cavan">Cavan</option>
-              <option value="Clare">Clare</option>
-              <option value="Cork">Cork</option>
-              <option value="Derry">Derry</option>
-              <option value="Donegal">Donegal</option>
-              <option value="Down">Down</option>
-              <option value="Dublin">Dublin</option>
-              <option value="Fermanagh">Fermanagh</option>
-              <option value="Galway">Galway</option>
-              <option value="Kerry">Kerry</option>
-              <option value="Kildare">Kildare</option>
-              <option value="Kilkenny">Kilkenny</option>
-              <option value="Laois">Laois</option>
-              <option value="Leitrim">Leitrim</option>
-              <option value="Limerick">Limerick</option>
-              <option value="Longford">Longford</option>
-              <option value="Louth">Louth</option>
-              <option value="Mayo">Mayo</option>
-              <option value="Meath">Meath</option>
-              <option value="Monaghan">Monaghan</option>
-              <option value="Offaly">Offaly</option>
-              <option value="Roscommon">Roscommon</option>
-              <option value="Sligo">Sligo</option>
-              <option value="Tipperary">Tipperary</option>
-              <option value="Tyrone">Tyrone</option>
-              <option value="Waterford">Waterford</option>
-              <option value="Westmeath">Westmeath</option>
-              <option value="Wexford">Wexford</option>
-              <option value="Wicklow">Wicklow</option>
+              {/* options */}
             </select>
           </div>
           <div className="text-center mt-4">
